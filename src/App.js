40bb0fc20b102ff0f16077b01bf8b3c3f0367675
42trustokerezi <trustokerezi42@gmail.com>
2021-04-07 16:46:12 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { useState }  from 'react';
+import Splash from "./Components/SplashScreeen"
+import {  ThemeProvider } from "styled-components";
+import './App.css';
+
+const LightTheme = {
+  pageBackground: "white",
+  titleColor: "#dc658b",
+  tagLineColor: "black"
+};
+
+const DarkTheme = {
+  pageBackground: "#282c36",
+  titleColor: "lightpink",
+  tagLineColor: "lavender"
+}
+
+const themes = {
+  light: LightTheme,
+  dark: DarkTheme,
+}
+
+function App() {
+  const [theme, setTheme] = useState("light")
+  return (
+    <>
+      <ThemeProvider theme={themes[theme]}>
+        <Splash theme={theme} setTheme={setTheme}/>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
